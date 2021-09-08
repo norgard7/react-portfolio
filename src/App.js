4dc798cc6a71eea7e7ef02c05a1d7df004b2bcad
Lauren Norgard <lauren.c.schumann@gmail.com>
2021-09-07 21:47:34 -0500

    updated app.js to have all the working routes. Currently, workings on localhost, need to check when deployed on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-// import Main from './components/Main/index';
-import DisplayPages from './components/displayPage';
-// import Footer from './components/footer/footer';
-// import Header from './components/Header';
+import About from './components/About/About';
+import Header from './components/Header';
+import {BrowserRouter as Router, Switch, Route }from 'react-router-dom';
+import Projects from './components/Projects/Projects';
+import Main from './components/Main/index';
+import Footer from './components/footer/footer';
+import Resume from './components/Resume/Resume';
+import Contact from './components/Contact/Contact';
+
+
 
 
 
 function App() {
   return (
-      <div className="App">
-          <div className="wrapper">
-            < DisplayPages />
-          </div>
+      <Router>
+      <div>
+         <Header />
+         <Switch>
+            <Route path="/Home" component= {Home} />
+            <Route path="/About" component= {About} />
+            <Route path="/Projects" component= {Projects} />
+            <Route path="/Resume" component= {Resume} />
+            <Route path="/Contactme" component= {Contact} />
+            <Route path ="/" exact component= {Main} />
+            <Route path ="/react-portfolio" component= {Main} />
+         </Switch>
+         <Footer />
       </div>
+      </Router>
   );
 }
 
+const Home = () => (
+  <div>
+    <Main />
+  </div>
+)
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,52 +3,30 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Logo from "../images/logo.png";
-// import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-// import Main from './Main/index';
-// import Projects from './Projects/Projects';
-// import About from './About/About';
-// import Contact from './Contact/Contact';
-// import Resume from './Resume/Resume';
+import { Link } from "react-router-dom";
+
  
 
-export default function Header({currentPage, handlePageChange}) {
+function Header() {
   return (
-    // <Router>
-        <div>
+<div>
     <Navbar id="navbar">
      <Container>
      <Navbar.Toggle aria-controls="basic-navbar-nav" />
      <Navbar.Collapse id="basic-navbar-nav">
-       <Nav className="justify-content-evenly w-100 align-items-center">    
-        <Nav.Link href="#Projects"onClick={() => handlePageChange('Projects')}>Projects</Nav.Link>
-        <Nav.Link href="#About" onClick={() => handlePageChange('About')}>About</Nav.Link>
-         <Nav.Link href="#Main" onClick={() => handlePageChange('Main')}><img alt="logo" src={Logo} width="250" height="100" className="d-inline-block align-top"/></Nav.Link>
-         <Nav.Link href="#Resume" onClick={() => handlePageChange('Resume')}>Resume</Nav.Link>
-        <Nav.Link href="#Contact" onClick={() => handlePageChange('Contact')}>Contact Me</Nav.Link> 
+       <Nav className="justify-content-evenly w-100 align-items-center">
+
+         <Link to="/Projects">Projects</Link>
+         <Link to="/About">About</Link>
+         <Link to= "/Home"><img alt="logo" src={Logo} width="250" height="100" className="d-inline-block align-top"/></Link>
+         <Link to="/Resume">Resume</Link>
+        <Link to="/Contactme">Contact Me</Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
 </Navbar>
-
-        {/* <Switch>
-          <Route exact path="/react-portfolio/">
-            <Main />
-          </Route>
-          <Route path="/react-portfolio/About">
-            <About />
-          </Route>
-          <Route path="/react-portfolio/Projects">
-            <Projects />
-          </Route>
-          <Route path="/react-portfolio/Resume">
-            <Resume />
-          </Route>
-          <Route path="/react-portfolio/Contactme">
-            <Contact />
-          </Route>
-        </Switch>
-      </div>
-    </Router> */}
-    </div>
+</div>
+   
   );
 }
+export default Header;
